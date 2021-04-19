a59c670eeecf19a04e5c0de6dd9756ad8623f8e1
LegchenkoSA <lsa73@yandex.ru>
2021-04-19 21:53:56 +1000

    First Step

--- src/com/company/lsa/lesson1/Main.java
+++ src/com/company/lsa/lesson1/Main.java
@@ -5,8 +5,10 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("--------Lesson1---------");
-        for (int i=0; i<5; i++) {
-            System.out.println("do something..");
+        for (int i=5; i>=0; i--) {
+            System.out.print("do something..   -");
+            System.out.printf("%2d",i);
+            System.out.println();
         }
         System.out.println("   ...");
         System.out.println("Something Done!");
