6570e04d7a612522c5f25e587d10a5598e355ee4
LegchenkoSA <lsa73@yandex.ru>
2021-04-19 20:46:11 +1000

    FirstStep

---
+++ src/com/company/lsa/lesson1/Main.java
@@ -0,0 +1,9 @@
+package com.company.lsa.lesson1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Lesson1");
+    }
+}
