cc149cbf1608e1b6cd9ea8c8079ff6b4e433e4ff
LegchenkoSA <lsa73@yandex.ru>
2021-04-19 21:03:13 +1000

    First Step

--- src/com/company/lsa/lesson1/Main.java
+++ src/com/company/lsa/lesson1/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Lesson1");
+        System.out.println("--------Lesson1---------");
+        for (int i=0; i<5; i++) {
+            System.out.println("do something..");
+        }
+        System.out.println("   ...");
+        System.out.println("Something Done!");
+        System.out.println("------------------------");
     }
 }
