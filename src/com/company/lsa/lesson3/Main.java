78458b63660dbec9a87682f00952da688999f921
LegchenkoSA <lsa73@yandex.ru>
2021-04-22 23:27:11 +1000

    Lab3

---
+++ src/com/company/lsa/lesson3/Main.java
@@ -0,0 +1,75 @@
+package com.company.lsa.lesson3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("=====Lesson3=====");
+
+        Scanner in = new Scanner(System.in);
+        double res = 0;
+        // считываем первое значение (проверку качества и формата ввода номеров делать не будем)
+        System.out.print("Input first number: ");
+        double num1 = in.nextDouble();
+
+        boolean opGood=false;
+        String[] ops = {"+", "-", "/", "*", "!"};   // доступные операции
+        String oper;
+        do {
+            // печатаем список доступных операций
+            System.out.print("Input operation (");
+            for (String op : ops) System.out.print(op);
+            System.out.print("): ");
+
+            // считываем операцию
+            oper = in.next();
+            // проверяем что операция в списке допустимых
+            for (String op : ops) {
+                opGood = oper.equals(op);
+                if (opGood) break;
+            }
+        } while (!opGood);  // выходим только если операция допустима
+
+        if (oper.equals("!")) {     // если операция факториал (унарная)
+            res=1;
+            // считаем факториал
+            int i;
+            for (i=1; i<=num1; i++)
+                res=res*i;
+            // выводим факториал, причем учитываем, что он у нас целый
+            System.out.printf("Your number: %d"+oper+" = %d\n", i-1, (int) res);
+        }
+        else {              // если операция бинарная
+            System.out.print("Input second number: ");
+            // считываем второй оператор
+            double num2 = in.nextDouble();
+            // вычисляем операцию
+            boolean err=false;
+            switch (oper) {
+                case "*":
+                    res = num1 * num2;
+                    break;
+                case "+":
+                    res = num1 + num2;
+                    break;
+                case "/":
+                    if (num2 != 0) {
+                        res = num1 / num2;
+                    } else {
+                        err = true;
+                        System.out.print("Divide by zero!!!");
+                    }
+                    break;
+                case "-":
+                    res = num1 - num2;
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + oper);
+            }
+
+            if (!err) System.out.printf("Your number: %.2f "+oper+" %.2f = %.2f\n", num1, num2, res);
+        }
+        in.close();
+    }
+}
