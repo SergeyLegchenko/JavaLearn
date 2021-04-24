1b4570a9c10a58dc2ef96773980caab979f679e6
SergeyLegchenko <58903512+SergeyLegchenko@users.noreply.github.com>
2021-04-24 15:39:13 +1000

    Update Main.java

--- src/com/company/lsa/lesson3/Main.java
+++ src/com/company/lsa/lesson3/Main.java
@@ -14,7 +14,7 @@ public class Main {
         double num1 = in.nextDouble();
 
         boolean opGood=false;
-        String[] ops = {"+", "-", "/", "*", "!"};   // доступные операции - тут
+        String[] ops = {"+", "-", "/", "*", "!"};   // доступные операции - указаны тут
         String oper;
         do {
             // печатаем список доступных операций
