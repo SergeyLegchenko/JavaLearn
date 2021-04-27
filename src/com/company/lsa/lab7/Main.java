cea44925d9c075e10e42ad9f4066c5fe7090bbfd
Denis Tsyganov <dtsyganov@luxoft.com>
2021-04-27 23:33:15 +0600

    lab 7 - ok. refactored a bit + formatting

--- src/com/company/lsa/lab7/Main.java
+++ src/com/company/lsa/lab7/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("======== Lab 7 =======");
+        MyArrayList<String> list = new MyArrayList<>();
+        list.add("1");
+        System.out.println(list.get(0));
     }
 }
--- src/com/company/lsa/lab7/MyArrayList.java
+++ src/com/company/lsa/lab7/MyArrayList.java
@@ -1,37 +1,40 @@
 package com.company.lsa.lab7;
 
+import java.util.Arrays;
+
 public class MyArrayList<T> {
-    private final int INIT_SIZE = 16;
-    private final int CUT_RATE = 4;
+    private static final int INIT_SIZE = 16;
+    private static final int CUT_RATE = 4;
     private Object[] array = new Object[INIT_SIZE];
     private int pointer = 0;
 
     public void add(T item) {
-        if(pointer == array.length-1)
-            resize(array.length*2);
+        if (pointer == array.length - 1)
+            resize(array.length * 2);
         array[pointer++] = item;
     }
 
     private void resize(int newLength) {
-        Object[] newArray = new Object[newLength];
-        System.arraycopy(array, 0, newArray, 0, pointer);
-        array = newArray;
+        array = Arrays.copyOf(array, newLength);
     }
 
     public T get(int index) {
-        return (T) array[index];
-     }
+        @SuppressWarnings("unchecked")
+        T t = (T) array[index];
+        return t;
+    }
 
     public int size() {
         return pointer;
     }
 
     public void remove(int index) {
-        if (pointer - index >= 0) System.arraycopy(array, index + 1, array, index, pointer - index);
-        array[pointer] = null;
-        pointer--;
+        if (pointer - index >= 0) {
+            System.arraycopy(array, index + 1, array, index, pointer - index);
+        }
+        array[pointer--] = null;
         if (array.length > INIT_SIZE && pointer < array.length / CUT_RATE)
-            resize(array.length/2);     // если элементов в CUT_RATE раз  меньше чем
-                                                  // длина массива, то уменьшу в два раза
+            resize(array.length / 2);     // если элементов в CUT_RATE раз  меньше чем
+        // длина массива, то уменьшу в два раза
     }
 }
