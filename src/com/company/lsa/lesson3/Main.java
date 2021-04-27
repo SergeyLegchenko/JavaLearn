13054f817df88b7f24e9872a82d0ff542d236c46
Denis Tsyganov <dtsyganov@luxoft.com>
2021-04-27 13:02:21 +0600

    lab 3 - ok. порефакторил.

--- src/com/company/lsa/lesson3/Main.java
+++ src/com/company/lsa/lesson3/Main.java
@@ -1,87 +1,82 @@
 package com.company.lsa.lesson3;
 
-import java.util.Locale;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+    private static final String[] OPERATIONS = {"+", "-", "/", "*", "!", "q"};   // доступные операции - указаны тут
+
     public static void main(String[] args) {
         // write your code here
         System.out.println("=====Lesson3=====");
 
         Scanner in = new Scanner(System.in);
-        double res = 0;
+        double result;
 
-        boolean opGood=false;
-        String[] ops = {"+", "-", "/", "*", "!", "q"};   // доступные операции - указаны тут
-        String oper="";
+        String operation = "";
         do {
             try {
-                do {
-                    // печатаем список доступных операций
-                    System.out.print("Input operation (");
-                    for (String op : ops) System.out.print(op);
-                    System.out.print("): ");
+                operation = getOperation(in);
 
-                    // считываем операцию
-                    oper = in.next();
-                    // проверяем что операция в списке допустимых
-                    for (String op : ops) {
-                        opGood = oper.toLowerCase().equals(op);
-                        if (opGood) break;
-                    }
-                } while (!opGood);  // выходим только если операция допустима
-
-                if (!oper.equalsIgnoreCase("q")) {
+                if (!operation.equalsIgnoreCase("q")) {
                     // считываем первое значение (проверку качества и формата ввода номеров делать не будем)
                     System.out.print("Input first number: ");
                     double num1 = in.nextDouble();
 
-                    if (oper.equals("!")) {     // если операция факториал (унарная)
-                        res = 1;
-                        // считаем факториал
-                        int i;
-                        for (i = 1; i <= num1; i++)
-                            res = res * i;
-                        // выводим факториал, причем учитываем, что он у нас целый
-                        System.out.printf("Your number: %d" + oper + " = %d\n", i - 1, (int) res);
+                    if (operation.equals("!")) {     // если операция факториал (унарная)
+                        System.out.printf("Your number: %d %s = %d%n", (int) num1, operation, (int) factorial(num1));
                     } else {              // если операция бинарная
                         System.out.print("Input second number: ");
                         // считываем второй оператор
                         double num2 = in.nextDouble();
                         // вычисляем операцию
-                        boolean err = false;
-                        switch (oper.toLowerCase()) {
-                            case "*":
-                                res = num1 * num2;
-                                break;
-                            case "+":
-                                res = num1 + num2;
-                                break;
-                            case "/":
-                                if (num2 != 0) {
-                                    res = num1 / num2;
-                                } else {
-                                    err = true;
-                                    System.out.print("Divide by zero!!!");
-                                }
-                                break;
-                            case "-":
-                                res = num1 - num2;
-                                break;
-                            default:
-                                throw new IllegalStateException("Unexpected value: " + oper);   // так вообще не бывает, но мало ли..
-                        }
-
-                        if (!err) System.out.printf("Your number: %.2f " + oper + " %.2f = %.2f\n", num1, num2, res);
+                        result = switch (operation) {
+                            case "*" -> num1 * num2;
+                            case "+" -> num1 + num2;
+                            case "/" -> num1 / num2;
+                            case "-" -> num1 - num2;
+                            default -> throw new IllegalStateException("Unexpected value: " + operation);   // так вообще не бывает, но мало ли..
+                        };
+                        System.out.printf("Your number: %.2f %s %.2f = %.2f%n", num1, operation, num2, result);
                     }
                 }
-            }
-            catch (InputMismatchException ex)
-            {
+            } catch (InputMismatchException ex) {
                 System.out.println("Wrong input number!");
+            } catch (ArithmeticException ex) {
+                System.out.print("Divide by zero!!!");
             }
-        } while (!oper.equalsIgnoreCase("q"));
+        } while (!operation.equalsIgnoreCase("q"));
         in.close();
+    }
+
+    private static double factorial(double num1) {
+        double result;
+        result = 1;
+        // считаем факториал
+        for (int i = 1; i <= num1; i++)
+            result = result * i;
+        // выводим факториал, причем учитываем, что он у нас целый
+        return result;
+    }
+
+    private static String getOperation(Scanner in) {
+        String operation;
+        do {
+            // печатаем список доступных операций
+            System.out.print("Input operation (");
+            for (String op : OPERATIONS) {
+                System.out.print(op);
+            }
+            System.out.print("): ");
+
+            // считываем операцию
+            operation = in.next();
+            // проверяем что операция в списке допустимых
+            for (String op : OPERATIONS) {
+                if (op.equals(operation)) {
+                    return operation;
+                }
+            }
+        } while (true);  // выходим только если операция допустима
     }
 }
