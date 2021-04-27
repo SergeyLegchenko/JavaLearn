44f574a219b97367fcffc1e6ef4288025e21afeb
Denis Tsyganov <dtsyganov@luxoft.com>
2021-04-27 23:27:41 +0600

    lab 6 - ok. refactored a bit + formatting

--- src/com/company/lsa/lab6/Main.java
+++ src/com/company/lsa/lab6/Main.java
@@ -1,10 +1,11 @@
 package com.company.lsa.lab6;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
-    static final int nCount = 5;
+    static final int MAX_PARCELS = 5;
 
     public static void main(String[] args) {
         // write your code here
@@ -12,19 +13,19 @@ public class Main {
         System.out.println("    Enter Products:");
         System.out.println("  : Name Cost Count <Enter> (or enter 'exit' or 'q' for exit)");
         System.out.println("=======================");
-        System.out.print(  "1 : ");
+        System.out.print("1 : ");
 
-        Parcel[] masOfParcel = new Parcel[nCount];
+        Parcel[] masOfParcel = new Parcel[MAX_PARCELS];
 
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
         int count = 0;
-        while(sc.hasNext() && count < nCount) {
+        while (sc.hasNext() && count < MAX_PARCELS) {
             String s = sc.nextLine();
-            if("exit".equalsIgnoreCase(s) || "q".equalsIgnoreCase(s)) {
+            if ("exit".equalsIgnoreCase(s) || "q".equalsIgnoreCase(s)) {
                 break;
             }
             count++;
-            System.out.print(count+1+" : ");
+            System.out.print(count + 1 + " : ");
 
             String[] parts = s.split(" ");
             if (parts.length != 3) {
@@ -38,26 +39,26 @@ public class Main {
             try {
                 productCost = Integer.parseInt(parts[1]);
                 productCount = Integer.parseInt(parts[2]);
-            } catch (NumberFormatException ex){
+            } catch (NumberFormatException ex) {
                 System.out.println(ex.getMessage());
                 System.out.println("Retry!");
                 continue;
             }
 
             boolean productAlreadyExists = false;
-            for (int i = 0; i < nCount; i++) {
-                if(masOfParcel[i] == null) continue;
-                if(productName.equalsIgnoreCase(masOfParcel[i].getProductName()) && (masOfParcel[i].getCosts() == productCost)) {
+            for (int i = 0; i < MAX_PARCELS; i++) {
+                if (masOfParcel[i] == null) continue;
+                if (productName.equalsIgnoreCase(masOfParcel[i].getProductName()) && (masOfParcel[i].getCosts() == productCost)) {
 //                    masOfParcel[i].setCosts(productCost);  //лишнее, т.к. мы цену уже сравнили - она такая же
-                    masOfParcel[i].addCounts(productCount) ;
+                    masOfParcel[i].addCounts(productCount);
                     productAlreadyExists = true;
                     break;
                 }
             }
 
-            if(!productAlreadyExists) {
-                for (int i = 0; i < nCount; i++) {
-                    if(masOfParcel[i] == null){
+            if (!productAlreadyExists) {
+                for (int i = 0; i < MAX_PARCELS; i++) {
+                    if (masOfParcel[i] == null) {
                         masOfParcel[i] = new Parcel(productName, productCost, productCount);
                         break;
                     }
@@ -66,38 +67,30 @@ public class Main {
         }
 
         System.out.println(Arrays.toString(masOfParcel));
-        Parcel[] sortedParcels = new Parcel[nCount];
-        System.arraycopy(masOfParcel, 0, sortedParcels, 0, nCount);
+        Parcel[] sortedParcels = new Parcel[MAX_PARCELS];
+        System.arraycopy(masOfParcel, 0, sortedParcels, 0, MAX_PARCELS);
 
-        Arrays.sort(sortedParcels, (p1, p2) -> {
-            if ((p1 == null) && (p2 == null))return 0;
-            if (p1 == null)return 1000;
-            if (p2 == null)return -1000;
-
-            return p1.compareTo(p2);
-        }
-        );
+        Arrays.sort(sortedParcels, Comparator.nullsLast(Comparator.naturalOrder()));
 
         System.out.println(Arrays.toString(sortedParcels));
 
         long sum = 0;
-        for (int i = 0; i < nCount; i++) {
+        for (int i = 0; i < MAX_PARCELS; i++) {
             if (masOfParcel[i] == null) continue;
             sum += masOfParcel[i].getValue();
         }
         System.out.print("Sum: ");
         System.out.println(sum);
 
-        TakePopular(masOfParcel);
-        for (int j = 0; j < 3; j++)
-            TakePopular(masOfParcel);
+        for (int j = 0; j <= 3; j++)
+            takePopular(masOfParcel);
     }
 
-    static void TakePopular (Parcel[] masOfParcel) {
+    static void takePopular(Parcel[] masOfParcel) {
         int indexOfMostPopularProduct = 0;
-        for (int i = 0; i < nCount; i++) {
+        for (int i = 0; i < MAX_PARCELS; i++) {
             if (masOfParcel[i] == null) continue;
-            if(masOfParcel[i].getCounts() > masOfParcel[indexOfMostPopularProduct].getCounts()) {
+            if (masOfParcel[i].getCounts() > masOfParcel[indexOfMostPopularProduct].getCounts()) {
                 indexOfMostPopularProduct = i;
             }
         }
